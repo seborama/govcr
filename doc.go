974d96b5207eb226af1d26a5282879451a6a2e32
Seb C <seborama@users.noreply.github.com>
2016-07-15 19:35:17 +0100

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,1 +1,7 @@
 
+/*
+Package govcr records and replays HTTP interactions for offline unit / behavioural / integration tests thereby acting as an HTTP mock.
+
+This project was inspired by php-vcr which is a PHP port of VCR for ruby.
+*/
+package govcr
