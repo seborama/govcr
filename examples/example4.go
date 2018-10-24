fe972295d2c1a4c4a4495e6c20dc807c7054d38a
seborama <seb.d.albion@outlook.com>
2018-10-25 00:16:06 +0100

    Removed last remains of ExcludeHeaderFunc and bug fixes

--- examples/example4.go
+++ examples/example4.go
@@ -14,13 +14,13 @@ const example4CassetteName = "MyCassette4"
 // The request contains a customer header 'X-Custom-My-Date' which varies with every request.
 // This example shows how to exclude a particular header from the request to facilitate
 // matching a previous recording.
-// Without the ExcludeHeaderFunc, the headers would not match and hence the playback would not
+// Without the RequestFilters, the headers would not match and hence the playback would not
 // happen!
 func Example4() {
 	vcr := govcr.NewVCR(example4CassetteName,
 		&govcr.VCRConfig{
 			RequestFilters: govcr.RequestFilters{
-				govcr.RequestDeleteHeaderKeys("X-Transaction-Id"),
+				govcr.RequestDeleteHeaderKeys("X-Custom-My-Date"),
 			},
 			Logging: true,
 		})
