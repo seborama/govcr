448e2ea90304fe3dad3c17bf55589a883ebd4399
Abdul Qadir <abdul.qadir@securiti.ai>
2019-09-24 12:10:30 +0500

    Example and Test files for SkipErrorCode feature

---
+++ examples/example8.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/seborama/govcr"
+)
+
+const example8CassetteName = "MyCassette8"
+
+// Example8 is an example use of SkipErrorCodes feature.
+func Example1() {
+	cfg := govcr.VCRConfig{
+		Logging: true,
+		SkipErrorCodes: true,
+	}
+	vcr := govcr.NewVCR(example1CassetteName, &cfg)
+	vcr.Client.Get("http://www.example.com/foo")
+	fmt.Printf("%+v\n", vcr.Stats())
+}
